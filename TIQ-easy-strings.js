c75646460360a5715cae433d47e816b60905c913
CarlieMartece <carliemartece@gmail.com>
2023-05-23 22:07:50 +0100

    install mocha and chai, and complete reverseString function

--- TIQ-easy-strings.js
+++ TIQ-easy-strings.js
@@ -1,3 +1,8 @@
-exports.reverseString = (S) => {
-    return S;
+exports.reverseString = (s) => {
+    const midPoint = Math.floor(s.length / 2) - 1;
+    for (let i = 0; i <= midPoint; i++) {
+        let j = s.length - 1 - i;
+        [s[i], s[j]] = [s[j], s[i]];
+    }
+    return s;
 };
--- __tests__/TIQ-easy-strings.test.js
+++ __tests__/TIQ-easy-strings.test.js
@@ -1,11 +1,32 @@
 const assert = require('assert');
+var expect = require('chai').expect;
 const {
     reverseString
 } = require("../TIQ-easy-strings");
 
 describe("Reverse String", () => {
-    it("returns a string", () => {
-        const actual = reverseString("A");
-        assert.ok(typeof actual === "string");
+    it("returns an array", () => {
+        const actual = reverseString(["A"]);
+        assert.equal(Array.isArray(actual), true);
+    });
+    it("reverses an even-lengthed string array", () => {
+        let newString = ["A", "B"];
+        let actual = reverseString(newString);
+        let expected = ['B', 'A'];
+        expect(actual).deep.to.equal(expected);
+        newString = ["H","a","n","n","a","h","B","a","n","a","n","a"];
+        actual = reverseString(newString);
+        expected = ["a","n","a","n","a","B","h","a","n","n","a","H"];
+        expect(actual).deep.to.equal(expected);
+    });
+    it("reverses an odd-lengthed string array", () => {
+        let newString = ["A", "B", "C"];
+        let actual = reverseString(newString);
+        let expected = ['C','B', 'A'];
+        expect(actual).deep.to.equal(expected);
+        newString = ["H","a","n","n","a","h","B","a","n","a","n","a","!"];
+        actual = reverseString(newString);
+        expected = ["!","a","n","a","n","a","B","h","a","n","n","a","H"];
+        expect(actual).deep.to.equal(expected);
     });
 });
